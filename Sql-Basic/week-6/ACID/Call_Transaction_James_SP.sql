b86991e743a73f8253742c3f7c6326180088e085
Natrayan <natrayan@ericsson.com>
2021-09-21 23:08:30 +0530

    views/sp/ACID

---
+++ Sql-Basic/week-6/ACID/Call_Transaction_James_SP.sql
@@ -0,0 +1,9 @@
+SELECT * FROM BankAccounts;
+
+SELECT * FROM ShoeShop;
+
+CALL TRANSACTION_JAMES(2);  -- Caller query
+
+SELECT * FROM BankAccounts;
+
+SELECT * FROM ShoeShop;
---
+++ Sql-Basic/week-6/ACID/Call_Transaction_Rose_SP.sql
@@ -0,0 +1,9 @@
+SELECT * FROM BankAccounts;
+
+SELECT * FROM ShoeShop;
+
+CALL TRANSACTION_ROSE;  -- Caller query
+
+SELECT * FROM BankAccounts;
+
+SELECT * FROM ShoeShop;
